7f808b26edbbb60cf195b170faa8f8368ae941c0
vimosh <vitaly.moshkov@norgesgruppen.no>
2022-12-26 03:20:15 +0100

    Added solution to Day 18, part 2. Incorrect.

--- 18/README.md
+++ 18/README.md
@@ -46,11 +46,16 @@ What is the surface area of your scanned lava droplet?
 
 ## Part 2
 
-Something seems off about your calculation. The cooling rate depends on exterior surface area, but your calculation also included the surface area of air pockets trapped in the lava droplet.
+Something seems off about your calculation. The cooling rate depends on exterior surface area, but your calculation also
+included the surface area of air pockets trapped in the lava droplet.
 
-Instead, consider only cube sides that could be reached by the water and steam as the lava droplet tumbles into the pond. The steam will expand to reach as much as possible, completely displacing any air on the outside of the lava droplet but never expanding diagonally.
+Instead, consider only cube sides that could be reached by the water and steam as the lava droplet tumbles into the
+pond. The steam will expand to reach as much as possible, completely displacing any air on the outside of the lava
+droplet but never expanding diagonally.
 
-In the larger example above, exactly one cube of air is trapped within the lava droplet (at 2,2,5), so the exterior surface area of the lava droplet is 58.
+In the larger example above, exactly one cube of air is trapped within the lava droplet (at 2,2,5), so the exterior
+surface area of the lava droplet is 58.
 
 ### Task
+
 What is the exterior surface area of your scanned lava droplet?
--- 18/day_18.py
+++ 18/day_18.py
@@ -3,31 +3,17 @@ Solution to the Day 18, Part 1 and 2.
 
 Returns:
     PART 1. Exposed surfaces:  3448
+    PART 2. External surfaces:  1698 (That's not the right answer; your answer is too low)
 """
 
 import logging
 import os
-from datetime import datetime
 
 
 def part_1():
     """
     Solution to Part 1.
     """
-    logging.info("Started: " + str(datetime.now()))
-
-    s = 1  # size of the side of the cube
-    surfaces = []
-
-    # Get a list of all the surfaces based on the input file
-    for line in input_lines:
-        coord = line.rstrip().split(",")
-        x = int(coord[0])
-        z = int(coord[2])
-        y = int(coord[1])
-        new_cube_surfaces = define_cube_surfaces(x, y, z, s)
-
-        surfaces += new_cube_surfaces
 
     # Count duplicates
     covered_sides_cnt = 0
@@ -39,17 +25,100 @@ def part_1():
         if dup_cnt != 1:
             covered_sides_cnt += dup_cnt
 
-    logging.info("Finished: " + str(datetime.now()))
-
     print("PART 1. Exposed surfaces: ", len(surfaces) - covered_sides_cnt)
 
 
-def define_cube_surfaces(x: int, y: int, z: int, s: int):
+def part_2():
+    """
+    Solution to Part 2. Incorrect.
+    """
+
+    # Flatten the coordinates by removing a specific axis
+    # Count unique surfaces
+
+    # The solution does not take into account hidden cavities
+
+    exterior_surfaces_cnt = 0
+
+    abcd_surfaces = []
+    for i in range(0, len(surfaces), 6):
+        abcd_surface = []
+        # remove coord z
+        for surface in surfaces[i]:
+            new_coord = (surface[0], surface[1])
+            abcd_surface.append(new_coord)
+        abcd_surfaces.append(abcd_surface)
+    exterior_surfaces_cnt += get_unique_surfaces_cnt(abcd_surfaces)
+
+    efgh_surfaces = []
+    for i in range(1, len(surfaces), 6):
+        efgh_surface = []
+        # remove coord z
+        for surface in surfaces[i]:
+            new_coord = (surface[0], surface[1])
+            efgh_surface.append(new_coord)
+        efgh_surfaces.append(efgh_surface)
+    exterior_surfaces_cnt += get_unique_surfaces_cnt(efgh_surfaces)
+
+    abef_surfaces = []
+    for i in range(2, len(surfaces), 6):
+        abef_surface = []
+        # remove coord y
+        for surface in surfaces[i]:
+            new_coord = (surface[0], surface[2])
+            abef_surface.append(new_coord)
+        abef_surfaces.append(abef_surface)
+    exterior_surfaces_cnt += get_unique_surfaces_cnt(abef_surfaces)
+
+    cdgh_surfaces = []
+    for i in range(3, len(surfaces), 6):
+        cdgh_surface = []
+        # remove coord y
+        for surface in surfaces[i]:
+            new_coord = (surface[0], surface[2])
+            cdgh_surface.append(new_coord)
+        cdgh_surfaces.append(cdgh_surface)
+    exterior_surfaces_cnt += get_unique_surfaces_cnt(cdgh_surfaces)
+
+    aceg_surfaces = []
+    for i in range(4, len(surfaces), 6):
+        aceg_surface = []
+        # remove coord x
+        for surface in surfaces[i]:
+            new_coord = (surface[1], surface[2])
+            aceg_surface.append(new_coord)
+        aceg_surfaces.append(aceg_surface)
+    exterior_surfaces_cnt += get_unique_surfaces_cnt(aceg_surfaces)
+
+    bdfh_surfaces = []
+    for i in range(5, len(surfaces), 6):
+        bdfh_surface = []
+        # remove coord x
+        for surface in surfaces[i]:
+            new_coord = (surface[1], surface[2])
+            bdfh_surface.append(new_coord)
+        bdfh_surfaces.append(bdfh_surface)
+    exterior_surfaces_cnt += get_unique_surfaces_cnt(bdfh_surfaces)
+
+    print("PART 2. External surfaces: ", exterior_surfaces_cnt)
+
+
+def get_unique_surfaces_cnt(coords: list[list[tuple[int, int]]]):
+    unique_coords = []
+    for c in coords:
+        if c not in unique_coords:
+            unique_coords.append(c)
+
+    return len(unique_coords)
+
+
+def define_cube_surfaces():
     """
     Return a list of coordinates of all surfaces of a cube of size s with
     starting coordinate (x, y, z).
     Returns list[list[tuple[int, int, int]]]
     """
+
     a = (x, y, z)
     b = (x + s, y, z)
     c = (x, y + s, z)
@@ -66,6 +135,7 @@ def define_cube_surfaces(x: int, y: int, z: int, s: int):
     cdgh = [c] + [d] + [g] + [h]
     aceg = [a] + [c] + [e] + [g]
     bdfh = [b] + [d] + [f] + [h]
+
     cube = [abcd] + [efgh] + [abef] + [cdgh] + [aceg] + [bdfh]
 
     return cube
@@ -88,5 +158,18 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, format="%(message)s")
     # logging.disable(logging.CRITICAL)
     input_lines = read_input("day_18_input.txt")
+
+    s = 1  # size of the side of the cube
+    surfaces = []
+
+    # Get a list of all the surfaces based on the input file
+    for line in input_lines:
+        coord = line.rstrip().split(",")
+        x = int(coord[0])
+        z = int(coord[2])
+        y = int(coord[1])
+        new_cube_surfaces = define_cube_surfaces()
+        surfaces += new_cube_surfaces
+
     part_1()
-    # part_2()
+    part_2()
